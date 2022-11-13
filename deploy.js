4f19065f885796041c81ac1f6166671d9b32550c
Chandra Prakash Tiwari <cptiwari20@gmail.com>
2022-11-13 15:46:08 +0530

    Updated the compile and test files

--- deploy.js
+++ deploy.js
@@ -16,8 +16,7 @@ const deploy = async () => {
     
         const result = await new web3.eth.Contract(JSON.parse(interface))
             .deploy({
-                data: bytecode,
-                arguments: ['Hi there!']
+                data: bytecode
             })
             .send({
                 from: getAccounts[0],
