f2eb5def61e0bb7ef06cb0556559dc825259b981
Chandra Prakash Tiwari <cptiwari20@gmail.com>
2022-11-13 16:19:39 +0530

    Added smart contract for lottery

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -1,9 +1,33 @@
 pragma solidity ^0.4.17;
 
 contract Lottery {
-    string public manager;
+    address public manager;
+    address[] public players;
     
-    constructor() {
+    function Lottery() public {
         manager = msg.sender;
     }
+
+    modifier restricted(){
+        require(msg.sender == manager);
+        _;
+    }
+    // method for players to enter into the game
+    function enter() public payable{
+        require(msg.value > 0.01 ether);
+        players.push(msg.sender);
+    }
+    // pick winner randomly
+    function pickWinner() public restricted {
+        uint index = random() % players.length;
+       players[index].transfer(this.balance);
+        //reset the players
+        players = new address[](0);
+    }
+
+    // generate a random number
+    function random() private returns(uint){
+       return uint(keccak256(block.difficulty, now, players));
+    }
+
 }
