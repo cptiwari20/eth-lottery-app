d9f61d799b9639ed76259c2b78da545a167750b1
Chandra Prakash Tiwari <cptiwari20@gmail.com>
2022-11-13 15:46:24 +0530

    updated the complile file

--- compile.js
+++ compile.js
@@ -3,9 +3,9 @@ const fs = require('fs');
 const solc = require('solc');
 
 //get the path of solidity file
-const inboxSolPath = path.resolve(__dirname, 'contracts', 'inbox.sol');
+const lotterySolPath = path.resolve(__dirname, 'contracts', 'Lottery.sol');
 //read the file
-const source = fs.readFileSync(inboxSolPath, 'utf8')
+const source = fs.readFileSync(lotterySolPath, 'utf8')
 
 //complile the file
-module.exports = solc.compile(source, 1).contracts[':Inbox'];
+module.exports = solc.compile(source, 1).contracts[':Lottery'];
