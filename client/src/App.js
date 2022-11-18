4bb8e3e4b1ec2ec57d9d631c4fea3aeca75473b9
Chandra Prakash Tiwari <cptiwari20@gmail.com>
2022-11-18 11:34:52 +0530

    Added enter to the contract method

--- client/src/App.js
+++ client/src/App.js
@@ -7,6 +7,10 @@ function App() {
   const [manager, setManager] =  useState('')
   const [balance, setBalance] =  useState('')
   const [players, setPlayers] =  useState([])
+  const [value, setValue] =  useState(0)
+  const [message, setMessage] =  useState('')
+
+  
   useEffect(() => {
     handleLotteryData()
   },[])
@@ -18,6 +22,22 @@ function App() {
     setManager(getManager)
     setPlayers(getPlayers)
     setBalance(getBalance)
+  }
+  const handleSubmit = async (e) =>{
+    e.preventDefault();
+    const accounts = await web3.eth.getAccounts();
+    setMessage('Sending your transaction...')
+    try {
+      await lottery.methods.enter().send({
+        value: web3.utils.toWei(value, 'ether'),
+        from: accounts[0]
+      });
+      setMessage('Successfully entered to the contract!')
+      
+    } catch (error) {
+      setMessage(error.message)
+    }
+
   }
 
 
@@ -31,6 +51,16 @@ function App() {
         The manager of this contract is <b>{manager}</b>. Currently there are {players.length} players in this Lottery.
         The overall balance of this lotter is {web3.utils.fromWei(balance, 'ether')} ether!
       </p>
+      <hr />
+      <form onSubmit={handleSubmit}>
+        <label>Enter the lottery contract: </label>
+        <input value={value} onChange={e => setValue(e.target.value)} type='text' placeholder='Enter the etherem amount'/>
+        <button type='submit'>Enter</button>
+      </form>
+
+      <hr />
+      <h2>{message}</h2>
+
     </div>
   );
 }
